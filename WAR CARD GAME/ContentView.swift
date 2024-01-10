b00ac295f13f3d7d0523f9d1f3f084f0bbb72ae8
dilki3 <dilkidinesha345@gmail.com>
2024-01-10 18:13:57 +0530

    Initial Commit

---
+++ WAR CARD GAME/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  WAR CARD GAME
+//
+//  Created by Dila Dinesha on 2024-01-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack{
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                
+                
+                Button(action: {
+                    deal()
+                }, label: {
+                     Image("button")
+                })
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                            
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                           
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+    
+            }
+        }
+    }
+    
+    func deal() {
+        //randomize the player card
+        var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String (playerCardValue)
+        
+        //randomize the cpu cards
+        var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String (cpuCardValue)
+        
+        //update the score
+        if playerCardValue > cpuCardValue{
+            //add 1 to player score
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            //add 1 to cpu score
+            cpuScore += 1
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WAR CARD GAME/WAR_CARD_GAMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAR_CARD_GAMEApp.swift
+//  WAR CARD GAME
+//
+//  Created by Dila Dinesha on 2024-01-05.
+//
+
+import SwiftUI
+
+@main
+struct WAR_CARD_GAMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
